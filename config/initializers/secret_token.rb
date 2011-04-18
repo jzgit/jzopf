6f593312e0fb35fdd230de7e443d0e8809d0d5ca
Jason Zopf <jasonzopf@gmail.com>
2011-04-17 22:43:17 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jzopf::Application.config.secret_token = 'b169282738e2c6b7e5257be26e09218d54ab5e7942e491f3232b9b818bd2fa9a561bae82b60867a73bebc80dbde9df39501b1a9346581e0c948c465dbf159eea'
